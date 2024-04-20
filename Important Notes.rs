ee54be2af6aa1d741a54ab56457e37c24d04fe6f
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-04-19 21:14:42 -0300

    Important notes v1
    Im so fucking sleepy wtf

---
+++ Important Notes.rs
@@ -0,0 +1,37 @@
+// Boolean = true, false
+// Integer = 1, 2, 50, 99, -2
+// Double / Float = 1.1, 5.5, 200.0001. 2.0
+// Character = 'a', 'b', '7', '$'
+// String = "Hello", "string", "this is a string"
+// These are the basic data types 
+
+// =============================== 
+
+// Variables:
+/* 
+
+Immutable by default, but can be mutable, this helps to improve the speed
+since the code doesn't need to think if that var is going to change, since
+its immutable
+
+
+*/
+
+let two = 2; // Integer type
+let hello = "hello"; // String
+let j = 'j'; // Character because of single quotation
+let my_half = 0.5; // Double or Float type 
+let mut my_name = "Bill" // mut is for mutable, later it can be assigned another value, for example, Nick and also -> this is a String
+let quit_program = false; // Boolean type
+let your_half = my_half; // My_Half is another variable, so we are assigning a var to another var, which here means 0.5
+
+/*
+
+Variables can be assigned to any value, including other variables
+And remember always, they are immutable by default here on Rust 
+
+*/
+
+// ===============================
+
+// Functions:
