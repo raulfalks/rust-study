3a68461eeb9e29a856cfae3035b0bfafd88265d3
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-05-03 20:07:09 -0300

    exercise1 trial

---
+++ Exercises/exercises/src/exercise1.rs
@@ -0,0 +1,10 @@
+// Exercise 1 - Functions
+// Function to display your first name
+// Function to display your last name
+
+fn main() {
+    let first_name: &str = "Raulzao";
+    let last_name: &str = "Apelao";
+
+    println!("My first name is {} and my last name is {}", first_name, last_name);
+}
