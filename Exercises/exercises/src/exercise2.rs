5b90072bedc0e0280ac7cb59edc51921f0474538
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-05-04 21:13:51 -0300

    update exercise2.rs

--- Exercises/exercises/src/exercise2.rs
+++ Exercises/exercises/src/exercise2.rs
@@ -1,1 +1,19 @@
-// Exercise 2 - 
+// Exercise 2 - Basic Arithmetic
+
+fn sub(a: i32, b: i32) -> i32{
+        a-b
+}
+
+fn main() {
+    
+    let sum = 2 + 2;
+    let value = 10 - 5;
+    let divison = 10 / 2; 
+    let mult = 5 * 5;
+
+    let five = sub(8, 3); 
+    
+    let rem = 6 % 3;
+    let rem2 = 6 % 4;
+}
+
