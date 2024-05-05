670e69df6f310efce253dc21531c659f143b7bc2
Raul Guedes Carlessi <raul.falks@gmail.com>
2024-05-04 21:20:39 -0300

    update exercise2.rs

--- Exercises/exercises/src/exercise2.rs
+++ Exercises/exercises/src/exercise2.rs
@@ -1,5 +1,6 @@
 // Exercise 2 - Basic Arithmetic
 
+/* 
 fn sub(a: i32, b: i32) -> i32{
         a-b
 }
@@ -12,8 +13,31 @@ fn main() {
     let mult = 5 * 5;
 
     let five = sub(8, 3); 
-    
+
     let rem = 6 % 3;
     let rem2 = 6 % 4;
 }
+*/ 
+
+// Use a function to add numbers together
+// Use a function to display the result
+// use the {:?} token in the println macro to display the result
+
+fn main a: i32, b: i32) -> i32 { 
+    a + b 
+}
+
+fn main() {
+    let result = sum(132, 27); 
+    display_result(result);
+}
+
+fn toma_resultado(result: i32) {
+
+    println! = "{:?}", result; 
+
+}
+
+
+
 
